d1b06b95a27d52989f3b7d94253b54da28027e1b
jollyjerr <jollyjerr@gmail.com>
2019-10-11 15:48:43 -0600

    adds todo interface

--- index.ts
+++ index.ts
@@ -1,7 +1,28 @@
 import axios from 'axios';
 
-const url = "https://jsonplaceholder.typicode.com/todos/1"
+const url = "https://jsonplaceholder.typicode.com/todos/1";
+
+interface Todo {
+    id: number;
+    title: string;
+    completed: boolean;
+}
 
 axios.get(url).then(res => {
-    console.log(res.data);
+    const todo = res.data as Todo;
+
+    //Todos have the following attributes:
+    //id
+    //title
+    //completed
+
+    const ID = todo.id;
+    const title = todo.title;
+    const finished = todo.finished;
+
+    console.log(`
+        The Todo with ID: ${ID}
+        Has the title: ${title}
+        Is it finished? ${finished}
+    `);
 });
